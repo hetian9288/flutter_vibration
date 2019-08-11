855cdcfc5cffa1c0d03ac0b5396e43130629eeef
hetian9288 <hetian9288@gmail.com>
2019-08-11 11:30:31 +0800

    Update VibrationPlugin.m

--- ios/Classes/VibrationPlugin.m
+++ ios/Classes/VibrationPlugin.m
@@ -1,8 +1,30 @@
 #import "VibrationPlugin.h"
-#import <vibration/vibration-Swift.h>
+#import <AudioToolbox/AudioToolbox.h>
 
 @implementation VibrationPlugin
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftVibrationPlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"vibration"
+                                     binaryMessenger:[registrar messenger]
+                                     ];
+    VibrationPlugin* instance = [[VibrationPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result
+{
+    if ([@"hasVibrator" isEqualToString:call.method]) {
+        result(TARGET_OS_SIMULATOR == 0);
+    } else if ([@"hasAmplitudeControl" isEqualToString:call.method]) {
+        result(TARGET_OS_SIMULATOR == 0);
+    } else if ([@"vibrate" isEqualToString:call.method]) {
+        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+    } else if ([@"cancel" isEqualToString:call.method]) {
+        result(NULL);
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+
 @end
